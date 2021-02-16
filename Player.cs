eb4f3134ea317a7d0526db03a3e474d7f95153c0
rzientara <rzientara96@gmail.com>
2021-02-16 14:05:14 -0600

    added methods for creating new players

--- Player.cs
+++ Player.cs
@@ -12,5 +12,25 @@ namespace Poker_Tournament_App
         int rankChips;
 
         Dictionary<string, int> winningHandCount = new Dictionary<string, int>();
+
+        public Player()
+        {
+            setName("");
+            setDateJoined(new DateTime(1900, 01, 01));
+            setBirthday(new DateTime(1900, 01, 01));
+            setHometown("");
+            setRankChips(0);
+        }
+
+        public Player(string inName, DateTime inDateJoined, DateTime inBirthday, string inHometown, int inRankChips)
+        {
+            setName(inName);
+            setDateJoined(inDateJoined);
+            setBirthday(inBirthday);
+            setHometown(inHometown);
+            setRankChips(inRankChips);
+        }
+
+
     }
 }
