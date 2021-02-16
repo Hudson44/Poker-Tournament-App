6fd2c7a9fb554323ca5105e1699074f902e109b7
James Munger <jmunger@lakelandcollege.edu>
2021-02-16 16:49:10 -0600

    change to Program.cs as main

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Poker_Tournament_App
+{
+    class Program
+    {
+        public static void Main (string[] args) {
+        string selection;
+
+        Console.WriteLine("Select an option:\n ");
+        Console.WriteLine("1: Tournaments");
+        Console.WriteLine("2: Players");
+        Console.WriteLine("3: Overall Stats");
+        selection = Console.ReadLine();
+        
+        if (selection == "1")
+        {
+            Console.WriteLine("Tournaments.");
+        }
+        else if(selection == "2")
+        {
+            Console.WriteLine("Players.");
+        }
+        else if(selection == "3")
+        {
+            Console.WriteLine("Overall Stats.");
+        }
+    }
+    }
+}
