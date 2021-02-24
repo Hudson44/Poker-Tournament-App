d93c04b573eb19185a605b3b5156c1db97a92889
rzientara <rzientara96@gmail.com>
2021-02-24 17:53:34 -0600

    ViewTournament now displays tournaments in groups of 10.

--- ViewTournaments.cs
+++ ViewTournaments.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Poker_Tournament_App
 {
@@ -8,18 +9,46 @@ namespace Poker_Tournament_App
     {   
         public static void View()
         {
-            string selection;
+            string selection = "";
+            int displayed = 0;
+            int displayAmount = 10;
+
             List<Tournament> Tournaments = GetTournamentData.GetData();
+            while (!(selection == "q")){
+                Console.Clear();
+                Console.WriteLine("Select a tournament by ID:\n");
+                Console.WriteLine("[n] Next [b] Back");
 
-            Console.Clear();
-            Console.WriteLine("Select a tournament by ID:\n");
+                if (displayed < Tournaments.Count){
+                    if (selection == "n"){
+                        displayed += displayAmount;
+                    }
+                }
 
-            foreach (Tournament tournament in Tournaments){
-            Console.WriteLine(tournament.TournamentID + " " + tournament.Name);
-            
+                if (displayed >= displayAmount){
+                    if (selection == "b"){
+                        displayed -= displayAmount;
+                    }
+                }
+
+                try{
+                    foreach (int i in Enumerable.Range(displayed, displayAmount)){
+                        Console.WriteLine(Tournaments[i].TournamentID + " " + Tournaments[i].Name);
+                    }
+                }
+                catch{
+                    
+                }
+
+                //print tournament information
+                /*foreach (Tournament tournament in Tournaments){
+                Console.WriteLine(tournament.TournamentID + " " + tournament.Name);
+                
+                }*/Console.WriteLine(displayed);
+
+                selection = Console.ReadLine();
             }
-
-            selection = Console.ReadLine();
+            
 
         }
     }
