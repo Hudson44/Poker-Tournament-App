e7bf92d580a8cc0e761d28b0584e258153755432
rzientara <rzientara96@gmail.com>
2021-02-24 14:46:55 -0600

    Added commenting to GetTournamentData.cs

--- GetTournamentData.cs
+++ GetTournamentData.cs
@@ -9,15 +9,18 @@ namespace Poker_Tournament_App
     {
         public static void GetData()
         {
+            //list of tournament instances
             List<Tournament> Tournaments = new List<Tournament>();
 
             Tournament newTournament;
+            
             using(StreamReader reader = new StreamReader(@"Poker League Sample Data - Tournaments.csv")){
                 //header row from csv file
                 string headerLine = reader.ReadLine();
                 string[] headers = headerLine.Split(',');
                 
                 while (!reader.EndOfStream){
+                    //tournament information from csv file
                     string line = reader.ReadLine();
                     string[] values = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                     newTournament = new Tournament(values[1], values[2], values[3], Int32.Parse(values[6]));
@@ -26,7 +29,7 @@ namespace Poker_Tournament_App
                 }
             }
 
-            //printing out the list
+            //printing tournament information
             foreach (Tournament tournament in Tournaments){
             Console.WriteLine(tournament);
             }
