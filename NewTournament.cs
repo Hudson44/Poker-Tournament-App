3a26221bf5f98136d3e27cffc15c8224365dfd47
rzientara <rzientara96@gmail.com>
2021-02-25 13:57:58 -0600

    NewTournament.cs takes user data and adds the new tournament to the list. Does not write to the csv.

--- NewTournament.cs
+++ NewTournament.cs
@@ -6,9 +6,25 @@ namespace Poker_Tournament_App
 {
     public class NewTournament
     {   
+        static List<string> prompts = new List<string>{"Name: ", "Date: ", "Location: ", "Max players: ", "ID: "};
+        static List<string> enteredData = new List<string>();
+        static Tournament newTournament;
+
         public static void New()
         {
+            Console.Clear();
+            Console.WriteLine("Enter tournament data:");
             
+            enteredData.Clear();
+            
+            foreach (string prompt in prompts){
+                Console.WriteLine("\n" + prompt);
+                enteredData.Add(Console.ReadLine());
+            }
+            
+            newTournament = new Tournament(enteredData[0], enteredData[1], enteredData[2], Int32.Parse(enteredData[3]), enteredData[4]);
+            
+            Console.ReadLine();
         }
     }
 }
