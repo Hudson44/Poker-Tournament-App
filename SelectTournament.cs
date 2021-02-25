f68b33c0dc73ad1734a785be9283d2c63ddf9ea7
rzientara <rzientara96@gmail.com>
2021-02-24 18:23:50 -0600

    Created SelectTournament class to make a tournament the current one.

---
+++ SelectTournament.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Poker_Tournament_App
+{
+    class SelectTournament
+    {   
+        public static void Select()
+        {
+
+        }
+    }
+}
