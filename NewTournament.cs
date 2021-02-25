f156cad866a79dde21b27f4ac4ccf939873cd44c
rzientara <rzientara96@gmail.com>
2021-02-25 10:02:32 -0600

    Created NewTournament.cs

---
+++ NewTournament.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Poker_Tournament_App
+{
+    class NewTournament
+    {   
+        public static void New()
+        {
+            
+        }
+    }
+}
