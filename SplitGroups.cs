9babc6a42b281bb056e926426049b6836f0b6098
rzientara <rzientara96@gmail.com>
2021-03-02 12:25:04 -0600

    Creates SplitGroup class

---
+++ SplitGroups.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Poker_Tournament_App{
+  public static class SplitGroups {
+    public static void Split() {
+
+    }
+  }
+
+}
