897fe37117de8b90582f9f0019a60d77484eb0e5
rzientara <rzientara96@gmail.com>
2021-03-04 11:46:37 -0600

    Makes SplitGroups split the entire player list into lists of 4 and print the names.

--- SplitGroups.cs
+++ SplitGroups.cs
@@ -1,12 +1,29 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Poker_Tournament_App{
-  public static class SplitGroups {
-    public static void Split() {
+  public class SplitGroups {
+    static int groupSize = 4;
+    public static List<List<Player>> groups = new List<List<Player>>(); 
 
-    }
+    public static void Split()/*List<List<float[]>> Split(List<float[]> locations, int nSize=30)*/
+    {
+      for (int i = 0; i < PlayerList.Players.Count; i += groupSize)
+      {
+        groups.Add(PlayerList.Players.GetRange(i, Math.Min(groupSize, PlayerList.Players.Count - i))); 
+      }
+
+      foreach (List<Player> group in groups){
+        foreach (Player player in group){
+          Console.WriteLine(player.Name);
+        }
+        Console.WriteLine("-------------");
+      }
+      
+
+      Console.ReadLine();
+    } 
   }
-
 }
