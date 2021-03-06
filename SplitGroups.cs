668883c602e71957f11c04f709a0bffce41118bc
rzientara <rzientara96@gmail.com>
2021-03-06 12:21:21 -0600

    Moves split player list to a seperate GroupList class.

--- SplitGroups.cs
+++ SplitGroups.cs
@@ -6,16 +6,16 @@ using System.Linq;
 namespace Poker_Tournament_App{
   public class SplitGroups {
     static int groupSize = 4;
-    public static List<List<Player>> groups = new List<List<Player>>(); 
 
-    public static void Split()/*List<List<float[]>> Split(List<float[]> locations, int nSize=30)*/
+
+    public static void Split()
     {
       for (int i = 0; i < PlayerList.Players.Count; i += groupSize)
       {
-        groups.Add(PlayerList.Players.GetRange(i, Math.Min(groupSize, PlayerList.Players.Count - i))); 
+        GroupList.Groups.Add(PlayerList.Players.GetRange(i, Math.Min(groupSize, PlayerList.Players.Count - i))); 
       }
 
-      foreach (List<Player> group in groups){
+      foreach (List<Player> group in GroupList.Groups){
         foreach (Player player in group){
           Console.WriteLine(player.Name);
         }
