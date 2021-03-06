2281a0e3975a5531e7b8daa6b477549a451188b1
rzientara <rzientara96@gmail.com>
2021-03-06 12:23:57 -0600

    Removes printing from the SplitGroup class.

--- SplitGroups.cs
+++ SplitGroups.cs
@@ -7,23 +7,12 @@ namespace Poker_Tournament_App{
   public class SplitGroups {
     static int groupSize = 4;
 
-
     public static void Split()
     {
       for (int i = 0; i < PlayerList.Players.Count; i += groupSize)
       {
         GroupList.Groups.Add(PlayerList.Players.GetRange(i, Math.Min(groupSize, PlayerList.Players.Count - i))); 
       }
-
-      foreach (List<Player> group in GroupList.Groups){
-        foreach (Player player in group){
-          Console.WriteLine(player.Name);
-        }
-        Console.WriteLine("-------------");
-      }
-      
-
-      Console.ReadLine();
     } 
   }
 }
