9abb5cd23f9846b1d7710be63af7065adb47deb4
rzientara <rzientara96@gmail.com>
2021-03-09 13:29:58 -0600

    Alters DisplayList in ViewTournaments to be a list instead of a list of lists.

--- ViewTournaments.cs
+++ ViewTournaments.cs
@@ -14,7 +14,7 @@ namespace Poker_Tournament_App
       int displayed = 0;
       int displayAmount = 10;
       
-      List<List<Tournament>> DisplayList = new List<List<Tournament>>();
+      List<Tournament> DisplayList = new List<Tournament>();
 
       while (!(selection == "q")){
         DisplayList.Clear();
@@ -37,13 +37,13 @@ namespace Poker_Tournament_App
         }
 
         //gets the tournaments for the displyed group and puts them in DisplayList
-        for (int i = 0; i < displayAmount; i += 1){
-          DisplayList.Add(TournamentList.Tournaments.GetRange(displayed*displayAmount, Math.Min(displayAmount, TournamentList.Tournaments.Count - displayed*displayAmount)));
+        foreach (Tournament tournament in TournamentList.Tournaments.GetRange(displayed*displayAmount, Math.Min(displayAmount, TournamentList.Tournaments.Count - displayed*displayAmount))){
+          DisplayList.Add(tournament);
         }
 
         //displays the name of each tournament in DisplayList in a numbered list
-        foreach (Tournament tournament in DisplayList[0]){
-          Console.WriteLine((DisplayList[0].IndexOf(tournament) + 1).ToString() + ". " + tournament.Name);
+        foreach (Tournament tournament in DisplayList){
+          Console.WriteLine((DisplayList.IndexOf(tournament) + 1).ToString() + ". " + tournament.Name);
         }
 
         //display user prompts
@@ -57,8 +57,8 @@ namespace Poker_Tournament_App
         }
 
         //select a tournament with SelectTournament if the number by it is entered
-        foreach (Tournament tournament in DisplayList[0]){
-          if (selection == (DisplayList[0].IndexOf(tournament) + 1 ).ToString()){
+        foreach (Tournament tournament in DisplayList){
+          if (selection == (DisplayList.IndexOf(tournament) + 1 ).ToString()){
             SelectTournament.Select(tournament);
           }
         }
