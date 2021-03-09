2d8c9c4399ac2ca5c1fbb5756a13d6f513f4cd5c
rzientara <rzientara96@gmail.com>
2021-03-09 17:29:26 -0600

    Makes EditTournament validate player counts.

--- EditTournament.cs
+++ EditTournament.cs
@@ -7,6 +7,9 @@ namespace Poker_Tournament_App
   //the user can select a tournament, choose a field, and edit it
   public class EditTournament
   {  
+    static bool valid = false;
+    static int converted;
+    static string playerCount;
     public static void Edit(Tournament tournament)
     {
       string toEdit = "";
@@ -19,6 +22,8 @@ namespace Poker_Tournament_App
         Console.WriteLine("\n1. Name \n2. Date \n3. Location \n4. Max players \n\n[q] quit");
 
         toEdit = Console.ReadLine();
+        
+        Console.Clear();
 
         switch(toEdit){
           case "1":
@@ -35,7 +40,21 @@ namespace Poker_Tournament_App
             break;
           case "4":
             Console.WriteLine("\nEnter new max player amount:");
-            tournament.MaxPlayers = Int32.Parse(Console.ReadLine());
+
+            //makes sure player count is a non negative intiger
+            while (!valid){
+              playerCount = Console.ReadLine();
+              if (int.TryParse(playerCount, out converted) && converted > 0){
+                tournament.MaxPlayers = Int32.Parse(playerCount);
+                valid = true;
+              }
+              else{
+                Console.Clear();
+                Console.WriteLine("\nError: invalid input");
+                Console.WriteLine("\nEnter new max player amount:");
+              }
+            }
+            valid = false;
             break;
         }
       }
