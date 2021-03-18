453c954a8d1913cc0ade1378d1a5f2ad8172da15
Hudson44 <74075590+Hudson44@users.noreply.github.com>
2021-03-18 13:59:28 -0500

    Revert "Stats"

--- Program.cs
+++ Program.cs
@@ -6,24 +6,17 @@ namespace Poker_Tournament_App
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main (string[] args) {
             string selection = "";
-            //load tournament data
-            if (!TournamentList.Tournaments.Any())
-            {
-                GetTournamentData.GetData();
-            }
-            //load Player data
-            if (!PlayerList.Players.Any())
-            {
-                GetPlayerData.GetData();
-            }
-            // TournamentList.Tournaments[0].RegisterPlayer(PlayerList.Players[0]);
-            // Console.Write(TournamentList.Tournaments[0].Registrants[0]);
-            // Console.ReadLine();
-            while (!(selection == "4"))
-            {
+
+            if (!TournamentList.Tournaments.Any()){
+                    GetTournamentData.GetData();
+                }
+            if (!PlayerList.Players.Any()){
+                    GetPlayerData.GetData();
+                }
+
+            while (!(selection == "4")){
                 Console.Clear();
                 Console.WriteLine("Select an option:\n ");
                 Console.WriteLine("1: Tournaments");
@@ -31,17 +24,16 @@ namespace Poker_Tournament_App
                 Console.WriteLine("3: Overall Stats");
                 Console.WriteLine("4: Save & Quit");
                 selection = Console.ReadLine();
-                
-
+            
                 if (selection == "1")
                 {
                     ViewTournament.View();
                 }
-                else if (selection == "2")
+                else if(selection == "2")
                 {
                     ViewPlayer.View();
                 }
-                else if (selection == "3")
+                else if(selection == "3")
                 {
                     Console.WriteLine("Overall Stats.");
                 }
