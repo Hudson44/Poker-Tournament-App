f994e7cc2dc1dd8b37c5582c5565a20d788ac327
rzientara <rzientara96@gmail.com>
2021-03-20 17:30:36 -0500

    Removes unused code from line 11 and adjusts indents in GetPlayerData.cs

--- GetPlayerData.cs
+++ GetPlayerData.cs
@@ -4,39 +4,37 @@ using System.IO;
 
 namespace Poker_Tournament_App
 {
-    public class GetPlayerData
-    {
-        public static void GetData()
-        {   //list of player instances
-            List<Player> Players = new List<Player>();
+  public class GetPlayerData
+  {
+    public static void GetData()
+    { 
+      Player newPlayer;
 
-            Player newPlayer;
+      using(StreamReader reader = new StreamReader(@"Poker League Sample Data - Players.csv"))
+      {
+        //Header row from csv file
+        string headerLine = reader.ReadLine();
+        string[] headers = headerLine.Split(',');
 
-            using(StreamReader reader = new StreamReader(@"Poker League Sample Data - Players.csv"))
-            {
-                //Header row from csv file
-                string headerLine = reader.ReadLine();
-                string[] headers = headerLine.Split(',');
-
-                while (!reader.EndOfStream)
-                {
-                    //Player information from csv file
-                    string line = reader.ReadLine();
-                    string[] values = line.Split(',');
-                    newPlayer = new Player((values[0] + "," + values[1]).Replace("\"", ""), values[3], values[4], values[5], 0);
-                    
-                    //Player winning hand from csv file
-                    foreach (string value in values)
-                    {
-                        if (value == "1"){
-                            int index = Array.IndexOf(values, value);
-                            newPlayer.updateWinningHandCount(headers[index-1]);
-                        }
-                    }
-
-                    PlayerList.Players.Add(newPlayer);
-                }
+        while (!reader.EndOfStream)
+        {
+          //Player information from csv file
+          string line = reader.ReadLine();
+          string[] values = line.Split(',');
+          newPlayer = new Player((values[0] + "," + values[1]).Replace("\"", ""), values[3], values[4], values[5], 0);
+          
+          //Player winning hand from csv file
+          foreach (string value in values)
+          {
+            if (value == "1"){
+              int index = Array.IndexOf(values, value);
+              newPlayer.updateWinningHandCount(headers[index-1]);
             }
+          }
+
+          PlayerList.Players.Add(newPlayer);
         }
+      }
     }
+  }
 }
