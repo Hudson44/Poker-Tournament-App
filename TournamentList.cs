6ce604acbf6af3ef12bcd8ef77d46f5e2eaeb2db
rzientara <rzientara96@gmail.com>
2021-03-20 17:23:40 -0500

    Converts NewTournament to a method of TournamentList.

--- TournamentList.cs
+++ TournamentList.cs
@@ -63,7 +63,7 @@ namespace Poker_Tournament_App
 
         //run NewTournament if "new" is entered
         if (selection == "new"){
-          NewTournament.New();
+          NewTournament();
         }
 
         //select a tournament with SelectTournament if the number by it is entered
@@ -73,6 +73,56 @@ namespace Poker_Tournament_App
           }
         }
       }
+    }
+
+    public static void NewTournament()
+    {
+      string enteredData;
+      bool valid = false;
+      int converted;
+      List<string> prompts = new List<string>{"ID: ", "Name: ", "Date: ", "Location: ", "Max players: "};
+      List<string> dataList = new List<string>();
+      Tournament newTournament;
+
+      //clear list of entered data
+      dataList.Clear();
+      
+      //collect user input
+      foreach (string prompt in prompts){
+        Console.Clear();
+        Console.WriteLine("Enter tournament data:");
+        Console.WriteLine("\n" + prompt);
+        enteredData = Console.ReadLine();
+
+        //makes sure player counts and IDs are non negative intigers
+        if (prompts.IndexOf(prompt) == 0 || prompts.IndexOf(prompt) == 4){
+          while (!valid){
+            if (int.TryParse(enteredData, out converted) && converted > 0){
+              valid = true;
+            }
+            else{
+              Console.Clear();
+              Console.WriteLine("Enter tournament data:");
+              Console.WriteLine("\nError: invalid input");
+              Console.WriteLine("\n" + prompt);
+              enteredData = Console.ReadLine();
+            }
+          }
+          valid = false;
+        }
+
+        dataList.Add(enteredData);
+      }
+      
+      //create new tournament with user data
+      newTournament = new Tournament(dataList[0], dataList[1], dataList[2], dataList[3], Int32.Parse(dataList[4]), "", "", "", "", "");
+      TournamentList.Tournaments.Add(newTournament);
+
+      Console.Clear();
+      Console.WriteLine("New tournament created:");
+      Console.WriteLine(newTournament);
+      
+      Console.ReadLine();
     }
   }
 }
