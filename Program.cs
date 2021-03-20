3bf6b3e90603d5be92b17b7dd8de555dd844be30
rzientara <rzientara96@gmail.com>
2021-03-20 16:53:52 -0500

    Converts                     ViewPlayer to a method of PlayerList.

--- Program.cs
+++ Program.cs
@@ -4,40 +4,40 @@ using System.Linq;
 
 namespace Poker_Tournament_App
 {
-    class Program
-    {
-        public static void Main (string[] args) {
-            string selection = "";
+  class Program
+  {
+    public static void Main (string[] args) {
+      string selection = "";
 
-            if (!TournamentList.Tournaments.Any()){
-                    GetTournamentData.GetData();
-                }
-            if (!PlayerList.Players.Any()){
-                    GetPlayerData.GetData();
-                }
+      if (!TournamentList.Tournaments.Any()){
+        GetTournamentData.GetData();
+      }
+      if (!PlayerList.Players.Any()){
+        GetPlayerData.GetData();
+      }
 
-            while (!(selection == "4")){
-                Console.Clear();
-                Console.WriteLine("Select an option:\n ");
-                Console.WriteLine("1: Tournaments");
-                Console.WriteLine("2: Players");
-                Console.WriteLine("3: Overall Stats");
-                Console.WriteLine("4: Save & Quit");
-                selection = Console.ReadLine();
-            
-                if (selection == "1")
-                {
-                    ViewTournament.View();
-                }
-                else if(selection == "2")
-                {
-                    ViewPlayer.View();
-                }
-                else if(selection == "3")
-                {
-                    Console.WriteLine("Overall Stats.");
-                }
-            }
+      while (!(selection == "4")){
+        Console.Clear();
+        Console.WriteLine("Select an option:\n ");
+        Console.WriteLine("1: Tournaments");
+        Console.WriteLine("2: Players");
+        Console.WriteLine("3: Overall Stats");
+        Console.WriteLine("4: Save & Quit");
+        selection = Console.ReadLine();
+      
+        if (selection == "1")
+        {
+          ViewTournament.View();
         }
+        else if(selection == "2")
+        {
+          PlayerList.ViewPlayer();
+        }
+        else if(selection == "3")
+        {
+          Console.WriteLine("Overall Stats.");
+        }
+      }
     }
+  }
 }
